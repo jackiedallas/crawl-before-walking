9f0e19f31d5b2f8bb9b29240333433e7795c2d48
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-08-12 14:41:48 -0500

    practiced adding user interaction to js script

--- js/external.js
+++ js/external.js
@@ -1,2 +1,10 @@
-console.log("hello from external js");
-alert("hey this is external");
+"use strict";
+
+alert("Welcome back!");
+// the following line will show the OK/CANCEL cofirm dialog
+var confirmed = confirm('Are you Jackie?')
+console.log(confirmed); // will be either true or false
+// below is prompt example
+var userInput = prompt('Where were you born?');
+console.log('The user was born in ' + userInput + '.');
+
