d5f7ba8c6383d28c8b4d6eb7754d5faee50fb945
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-08-16 11:35:27 -0500

    practicing on practice branch

--- js/practice.js
+++ js/practice.js
@@ -1,6 +1,36 @@
 'use strict'
 
-var isAWeekDay = confirm("Is it a weekday?");
-var between9_5 = confirm("Is it between 9 AM and 5 PM?")
+function inClass() {
+	var isaWeekday = confirm("Is it a weekday?")
+	var between9_5 = confirm("Is it between 9am and 5pm?")
 
-console.log("Is Jackie in class? " + (isAWeekDay && between9_5))
+	if (isaWeekday && between9_5) {
+		alert("Jackie is in class.")
+	} else {
+		alert("Jackie is not in class.")
+	}
+}
+inClass();
+// var isAWeekDay = confirm("Is it a weekday?");
+// var between9_5 = confirm("Is it between 9 AM and 5 PM?")
+// var classMeet = confirm("Are you meeting on the weekend?")
+//
+// console.log("Is Jackie in class? " + ((isAWeekDay && between9_5) || classMeet))
+
+
+
+function moisterize() {
+	var isAshy = confirm("Are you ashy?")
+	var hasLotion = confirm("Do you have lotion?")
+
+	if (isAshy && hasLotion) {
+		alert("Don't forget to moisterize!")
+	} else if (isAshy && !hasLotion) {
+		alert("Go to the store and get some lotion!")
+	} else if (!isAshy && hasLotion) {
+		alert("Way to stay moisterized!")
+	} else {
+		alert("Well if you're not ashy, then you don't have a need for lotion.")
+	}
+}
+moisterize();
