7bd2ca669d711d3030b2153fad898849c918861a
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-10-14 08:59:43 -0500

    working on fizzbuzz

--- js/token_coding.js
+++ js/token_coding.js
@@ -1,2 +1,23 @@
 'use strict';
 
+$(document).ready(function () {
+	// alert("I'm ready")
+	
+	var loveScore = Math.random() * 100;
+	loveScore = Math.floor(loveScore) + 1;
+	// console.log(loveScore);
+	// alert('Your love score is ' + loveScore + '%.')
+	
+	var output = [];
+	function fizzBuzz() {
+		for (let i = 0; i < 100; i++) {
+			
+		}
+	}
+	
+	
+	
+	
+	
+})
+
